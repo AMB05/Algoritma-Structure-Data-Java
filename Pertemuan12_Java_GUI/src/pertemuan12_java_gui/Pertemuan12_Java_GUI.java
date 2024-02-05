cd567682e8b8fd53b68c05ef4b104b6ee15f0260
Arriva <92034212+AMB05@users.noreply.github.com>
2024-02-06 02:43:52 +0700

    GUI
    GUI

---
+++ Pertemuan12_Java_GUI/src/pertemuan12_java_gui/Pertemuan12_Java_GUI.java
@@ -0,0 +1,10 @@
+package pertemuan12_java_gui;
+
+public class Pertemuan12_Java_GUI {
+    
+    public static void main(String[] args) {
+        GUI_1 gui_1 = new GUI_1();
+        gui_1.setVisible(true);
+    }
+    
+}
---
+++ Pertemuan12_Java_GUI_Karyawan/src/pertemuan12_java_gui_karyawan/Pertemuan12_Java_GUI_Karyawan.java
@@ -0,0 +1,10 @@
+package pertemuan12_java_gui_karyawan;
+
+public class Pertemuan12_Java_GUI_Karyawan {
+
+    public static void main(String[] args) {
+        GUI_1 gui_1 = new GUI_1();
+        gui_1.setVisible(true);
+    }
+    
+}
